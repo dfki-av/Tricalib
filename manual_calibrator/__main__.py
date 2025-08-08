432ddcd850a858ca48e2e720353485b89f03ea57
Rahul Jakkamsetty <rahul.jakkamsetty@dfki.de>
2025-08-08 10:29:53 +0200

    updated __main__.py

---
+++ manual_calibrator/__main__.py
@@ -0,0 +1,16 @@
+__author__ = "Rahul Jakkamsetty"
+__license__ = "MIT"
+__doc__ = """
+A manual calibration tool for three modalities RGB, LiDAR and Event Images. Developed at DFKI 2025.
+"""
+#python imports
+
+# third-party imports
+# internal imports
+from manual_calibrator.gui.maingui import main
+
+
+
+
+if __name__ == "__main__":
+    main()
